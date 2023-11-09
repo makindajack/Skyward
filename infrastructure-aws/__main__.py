bdac8b609a06d986e36e612308ffde0cf13eea9a
SJA <adodey1@yahoo.com>
2023-11-09 23:58:42 +0000

    added the new code for infrastructure deployment

--- infrastructure-aws/__main__.py
+++ infrastructure-aws/__main__.py
@@ -1,25 +1,25 @@
 """An AWS Python Pulumi program"""
 
 
-# from eb import eb_ecs
+from eb import eb_ecs
 
 
-# conn = eb_ecs.create_rds()
+conn = eb_ecs.create_rds()
 
-# eb_ecs.create_elastic_beanstalk_with_ecs(conn)
-
-
-
-from ec2.ec2 import launch_template, create_auto_scaling_group
-from vpc.vpc import create_custom_vpc_and_subnets
+eb_ecs.create_elastic_beanstalk_with_ecs(conn)
 
 
 
-(custom_vpc, subnet_a, subnet_b, igw, route_table,
- rt_assoc_a, rt_assoc_b, custom_sg) = create_custom_vpc_and_subnets()
+# from ec2.ec2 import launch_template, create_auto_scaling_group
+# from vpc.vpc import create_custom_vpc_and_subnets
 
-subnet_ids = [subnet_a.id, subnet_a.id]
-custom_sg_id= custom_sg.id
+
+
+# (custom_vpc, subnet_a, subnet_b, igw, route_table,
+#  rt_assoc_a, rt_assoc_b, custom_sg) = create_custom_vpc_and_subnets()
+
+# subnet_ids = [subnet_a.id, subnet_a.id]
+# custom_sg_id= custom_sg.id
 
 
 lt = launch_template(custom_sg_id)
