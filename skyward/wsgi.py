f34b8ff030a00e80d95aa55190ff1182e9a5a7b7
Jackson Makinda <me@makindajackson.com>
2023-11-10 13:34:24 +0300

    wip: renaming
    + matching project name
    - folders renaming
    + updated html

--- skyward/wsgi.py
+++ skyward/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_con_project project.
+WSGI config for skyward project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_con_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skyward.settings')
 
 application = get_wsgi_application()
